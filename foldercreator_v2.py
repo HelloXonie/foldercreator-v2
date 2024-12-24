e120e17ae826141301cb3bfaef54e676e91e3dc7
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2024-12-23 22:07:54 -0800

    additional features. Needs debugging. Will continue tommorrow

--- foldercreator_v2.py
+++ foldercreator_v2.py
@@ -2,4 +2,17 @@ import sys
 import os
 from datetime import date
 
+foldername = (input ('Folder Name?'))
 
+
+os.chdir = ('~/code') #use absolute path to directory you want to create folders in.
+
+    try:
+        os.mkdir (foldername)
+        print('Folder Created')
+
+    except FileExistsError:
+        print('Folder already exists')
+
+    finally:
+        file = open('README.md', 'w+') 
